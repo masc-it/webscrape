5304b66b82be8790bab7a9b2e62b81efb981bc29
onipot <m.sciancalepore20@studenti.uniba.it>
2022-10-29 10:19:08 +0200

    proxy setup - intercept requests

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,30 @@
 mod webscrape;
 
-use headless_chrome::{Browser, LaunchOptions};
+use headless_chrome::{Browser, LaunchOptions, Element};
 use scraper::{Html, Selector};
-use webscrape::pipeline::ActionData;
+use webscrape::{pipeline::ActionData, scraper::Scraper};
 
 use crate::webscrape::pipeline::Actionable;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let f = std::fs::File::open("config_ip.yaml")?;
 
-    let obj: webscrape::pipeline::PipelineConfig = serde_yaml::from_reader(f).unwrap();
+    let mut scraper = Scraper::default();
 
-    let targets = &obj.targets;
+    scraper.navigate_to(&String::from("https://httpbin.org/ip"));
 
-    let proxy_url = format!("--proxy-server=http://{}:{}", "45.192.141.97", "5134"); //`http://${this.randomProxy.hostname}:${this.randomProxy.port}`
-    
-    let no_sandbox = "--no-sandbox";
-    let proxy_arg = std::ffi::OsString::from(proxy_url);
+    let elements = scraper.find_elements_by_css(&String::from("body"));
 
-    let sandbox_arg = std::ffi::OsString::from(no_sandbox);
-    let browser = Browser::new(LaunchOptions{
-
-        args: vec![&proxy_arg],
-        headless: false,
-        ..Default::default()
-    }).unwrap();
-    
-    let tab = &browser.wait_for_initial_tab()?;
-
-    tab.enable_fetch(None, Some(true)).unwrap();
-    tab.authenticate(Some(String::from("proxygobrr69")), Some(String::from("dksfjlfnajd32429"))).unwrap();
-
-    tab.navigate_to(&obj.pipeline.url).unwrap();
-
-    tab.wait_until_navigated().unwrap();
-    /* let el = tab.find_element_by_xpath("//a[contains(@href, 'lastact')]").unwrap();
-
-    println!("{}", el.get_inner_text().unwrap()); */
-
-    for t in targets.to_owned() {
-
-        tab.wait_for_elements_by_xpath(&t.selector).unwrap();
-        let r = t.find_element(tab);
-
-        println!("{}", r);
+    for el in elements {
+        println!("{:?}", el.get_inner_text().unwrap());
     }
-    /* for a in &actions {
-        let d = &a.data;
-        
-        match d {
-            // TODO try to flatten action payload in pipeline.rs
-            ActionData::ActionClick(e) => e.run(tab),
-            ActionData::ActionWait(e) => e.run(tab)
-        };
 
-    } */
-   
-    
-    //println!("{:?}", obj.scraping_actions.)
-    /* let s = webscrape::scraper::Scraper{
+    println!("XPATH..");
+    let elements = scraper.find_elements_by_xpath(&String::from("//body"));
 
-        persistent: true,
-        proxy: Proxy{url: "".to_owned()}
-    };
-    
-    let resp = reqwest::blocking::get("https://download.geofabrik.de/europe.html")?
-        .text()?;
-        
-    let mut urls : Vec<(String, String)> = Vec::default();
-    
-    let fragment = Html::parse_fragment(&resp);
-    let selector = Selector::parse("a[href*=\"-latest.osm.pbf\"]").unwrap();
-
-    for element in fragment.select(&selector) {
-
-        let el = element.value();
-        
-        let url = el.attr("href").unwrap().to_owned();
-
-        let url_parts: Vec<_> = el.attr("href").unwrap().split("/").collect();
-        if url_parts.len() < 2 {
-            continue;
-        }
-
-        let name = url_parts.get(1).unwrap().to_owned().to_owned();
-        urls.push((name, url));
+    for el in elements {
+        println!("{:?}", el.get_inner_text().unwrap());
     }
-    
-    for (name, url) in &urls {
-        println!("{} - {}", name, url)
-    } */
 
     Ok(())
 }
--- src/webscrape/scraper.rs
+++ src/webscrape/scraper.rs
@@ -1,15 +1,150 @@
-use super::proxy::Proxy;
+use std::sync::Arc;
+
+use headless_chrome::{Browser, LaunchOptions, Tab, Element, browser::{transport::{Transport, SessionId}, tab::RequestPausedDecision}, protocol::cdp::{Fetch::{events::RequestPausedEvent, HeaderEntry, FulfillRequest, RequestPattern, RequestStage}, Network::ResourceType}};
+
+use super::{proxy::Proxy, pipeline::Target};
 
 pub struct Scraper {
 
+    pub init_on_create: bool,
     pub persistent: bool,
-    pub proxy: Proxy
+    pub proxy: Vec<Proxy>,
+
+    browser: Browser,
+    tab: Arc<Tab>,
+
+    current_url: Option<String>
+}
+
+impl Default for Scraper {
+    fn default() -> Self {
+
+        let browser = Browser::new(LaunchOptions{
+
+            //args: vec![&proxy_arg],
+            headless: false,
+            ..Default::default()
+        }).unwrap();
+        
+        let tab = browser.wait_for_initial_tab().unwrap().to_owned();
+
+        let patterns = vec![
+            RequestPattern {
+                url_pattern: None,
+                resource_Type: None,
+                request_stage: Some(RequestStage::Response),
+            },
+            RequestPattern {
+                url_pattern: None,
+                resource_Type: None,
+                request_stage: Some(RequestStage::Request),
+            },
+        ];
+        tab.enable_fetch(Some(&patterns), None).unwrap();
+
+        tab.enable_request_interception(Arc::new(
+            move |transport: Arc<Transport>, session_id: SessionId, intercepted: RequestPausedEvent| {
+                
+                //println!("{:?}", intercepted.params.resource_Type);
+                // !intercepted.params.request.url.ends_with(".jpg") && !intercepted.params.request.url.ends_with(".png") && !intercepted.params.request.url.ends_with(".js")
+                if intercepted.params.resource_Type == ResourceType::Document {
+                let proxy = format!("http://{}:{}", "45.192.146.173", "6184");
+                
+                println!("running reqwest.. {}", intercepted.params.request.url);
+                let res = reqwest::blocking::Client::builder()
+                    .proxy(
+                        reqwest::Proxy::all(&proxy)
+                            .unwrap()
+                            .basic_auth("proxygobrr69", "dksfjlfnajd32429"),
+                    )
+                    .build()
+                    .unwrap()
+                    .get(intercepted.params.request.url).send().unwrap().text().unwrap();
+                    
+                    let headers = vec![HeaderEntry {
+                        name: "Content-Type".to_string(),
+                        value: "text/html; charset=utf-8".to_string(),
+                    }];
+    
+                    let fulfill_request = FulfillRequest {
+                        request_id: intercepted.params.request_id,
+                        response_code: 200,
+                        response_headers: Some(headers),
+                        binary_response_headers: None,
+                        body: Some(base64::encode(res)),
+                        response_phrase: None,
+                    };
+    
+                    RequestPausedDecision::Fulfill(fulfill_request)
+                } else {
+                    RequestPausedDecision::Continue(None)
+                }
+            },
+        )).unwrap();
+        Self { init_on_create: false, persistent: false, proxy: vec![], browser: browser, tab: tab, current_url: None }
+    }
 }
 
 impl Scraper {
 
-    fn load_url(url: String, render_js: bool) -> String {
-        "".to_owned()
+    pub fn navigate_to(&mut self, url: &String) -> bool {
+
+        self.tab.navigate_to(url).unwrap();
+
+        self.tab.wait_until_navigated().unwrap();
+
+        self.current_url = Some(url.to_string());
+        
+        return true;
+        
+    }
+
+    pub fn find_elements_by_css(&self, target: &String ) -> Vec<Element> {
+        
+        if target.starts_with("/") {
+            println!("Invalid CSS selector.");
+            return vec![];
+        }
+
+        if let None = self.current_url {
+            println!("Didn't you call navigate_to(url) ?");
+
+            return vec![];
+        }
+
+        let query_result = self.tab.wait_for_elements(&target);
+
+        let elements = match query_result {
+            Ok(elements) => elements,
+            Err(_) => vec![]
+        };
+
+        return elements;
+
+    }
+
+    pub fn find_elements_by_xpath(&self, target: &String ) -> Vec<Element> {
+        
+        if !target.starts_with("/") {
+            println!("Invalid XPath selector.");
+            return vec![];
+        }
+
+        if let None = self.current_url {
+            println!("Didn't you call navigate_to(url) ?");
+
+            return vec![];
+        }
+
+        let query_result = self.tab.wait_for_elements_by_xpath(&target);
+
+        let elements = match query_result {
+            Ok(elements) => elements,
+            Err(_) => vec![]
+        };
+
+        return elements;
+
     }
 
 }
