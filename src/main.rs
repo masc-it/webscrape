dbd5098be613361205a11da2a3d13e049868cd99
onipot <m.sciancalepore20@studenti.uniba.it>
2022-10-29 20:51:35 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 mod webscrape;
 
-use headless_chrome::{Browser, LaunchOptions, Element};
-use scraper::{Html, Selector};
-use webscrape::{pipeline::ActionData, scraper::Scraper};
+use crate::webscrape::{scraper::{ScraperBuilder, Builder}, proxy::SimpleProxy};
 
-use crate::webscrape::pipeline::Actionable;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 
-    let mut scraper = Scraper::default();
+    let proxies = SimpleProxy::from_csv(
+        "".to_string(), 
+        ":".to_string(), 
+        0, 
+        1, 
+        2, 
+        3
+    );
+
+    let mut builder = ScraperBuilder::default();
+
+    let mut scraper = builder
+        .set_default_timeout(5)
+        .set_proxies(proxies)
+        .build();
 
     scraper.navigate_to(&String::from("https://httpbin.org/ip"));
 
@@ -26,12 +37,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("{:?}", el.get_inner_text().unwrap());
     }
 
+
     println!("--------------------");
-    scraper.navigate_to(&String::from("https://stackoverflow.com/questions/58787864/changing-primary-palette-color-when-using-kivymd-has-no-effect-on-buttons"));
+    scraper.navigate_to(&String::from("https://whatismycountry.com/"));
 
     println!("XPATH..");
-    let elements = scraper.find_elements_by_xpath(&String::from("//a[contains(@href, 'lastact')]"));
+    let elements = scraper.find_elements_by_xpath(&String::from("//*[@id=\"country\"]"));
 
+    println!("{}", elements.len());
     for el in elements {
         println!("{:?}", el.get_inner_text().unwrap());
     }
