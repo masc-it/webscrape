bf504de6deb9e85de18e99a246fb8e8b24bbe01a
onipot <m.sciancalepore20@studenti.uniba.it>
2022-10-30 17:04:20 +0100

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,71 @@
 mod webscrape;
 
-use crate::webscrape::{scraper::{ScraperBuilder, Builder}, proxy::SimpleProxy};
+use crate::webscrape::{scraper::{ScraperBuilder, Builder}, proxy::{CSVProxyListBuilder, FromCSVBuilder, ProxyField}};
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+    let mut csv_proxylist_builder = CSVProxyListBuilder::default();
 
-    let proxies = SimpleProxy::from_csv(
-        "".to_string(), 
-        ":".to_string(), 
-        0, 
-        1, 
-        2, 
-        3
-    );
+    let proxies = csv_proxylist_builder
+        .set_source("")
+        .set_separator(":".to_string())
+        .set_columns([ProxyField::Host, ProxyField::Port, ProxyField::Username, ProxyField::Password])
+        .build();
 
     let mut builder = ScraperBuilder::default();
 
     let mut scraper = builder
+        .set_headless(true)
         .set_default_timeout(5)
         .set_proxies(proxies)
         .build();
 
-    scraper.navigate_to(&String::from("https://httpbin.org/ip"));
+    /* let elements = scraper
+        .navigate_to("https://httpbin.org/ip")
+        .find_elements_by_css("body",  "body")
+        .collect();
 
-    let elements = scraper.find_elements_by_css(&String::from("body"));
-
-    for el in elements {
-        println!("{:?}", el.get_inner_text().unwrap());
-    }
-
-    println!("XPATH..");
-    let elements = scraper.find_elements_by_xpath(&String::from("//body"));
-
-    for el in elements {
-        println!("{:?}", el.get_inner_text().unwrap());
-    }
-
+    for (name, el) in elements {
+        println!("{:?}", el.text);
+        
+    } */
 
     println!("--------------------");
-    scraper.navigate_to(&String::from("https://whatismycountry.com/"));
 
-    println!("XPATH..");
-    let elements = scraper.find_elements_by_xpath(&String::from("//*[@id=\"country\"]"));
+    let elements = scraper
+        .navigate_to("https://stackoverflow.com/questions/58787864/changing-primary-palette-color-when-using-kivymd-has-no-effect-on-buttons")
+        .find_elements_by_xpath("last_activity", "//a[contains(@href, 'lastact')]")
+        .find_elements_by_xpath("question", "//h1/a[contains(@class, 'question-hyperlink')]")
+        .find_elements_by_xpath("views", "//div[contains(@title, 'Viewed')]")
+        .collect();
 
-    println!("{}", elements.len());
-    for el in elements {
-        println!("{:?}", el.get_inner_text().unwrap());
+    for (name, el) in elements {
+        println!("{:?}", el.text);
+
+        println!("{:?}", el.attrs.get("id").unwrap_or(&"NONE".to_string()));
+
+        for (k, v) in el.attrs {
+            println!("{} - {}", k, v);
+        }
     }
+    
+    // let elements = scraper
+    //     .navigate_to("https://whatismycountry.com/")
+    //     .find_elements_by_xpath("country", "//*[@id='country']")
+    //     .collect();
 
+    /* for (name, el) in elements {
+        println!("{:?}", el.text);
+
+        println!("{:?}", el.attrs.get("id").unwrap_or(&"NONE".to_string()));
+
+        for (k, v) in el.attrs {
+            println!("{} - {}", k, v);
+        }
+    } */
+
+    
 
     Ok(())
 }
