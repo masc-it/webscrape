1a136aa32bccb36522e9326c48d67dc006899176
onipot <m.sciancalepore20@studenti.uniba.it>
2022-10-30 10:15:53 +0100

    proxybuilder

--- src/webscrape/proxy.rs
+++ src/webscrape/proxy.rs
@@ -1,5 +1,115 @@
-use std::{io::{self, BufRead}, fs::File};
+use std::{io::{self, BufRead}, fs::File, collections::HashMap};
 
+#[derive(Eq, Hash, PartialEq, Default)]
+pub enum ProxyField {
+    #[default] Host,
+    Port,
+    Username,
+    Password
+}
+
+pub trait FromCSVBuilder {
+
+    fn set_source(&mut self, file_or_url: String) -> &mut CSVProxyListBuilder;
+
+    fn set_columns(&mut self, columns: [ProxyField; 4]) -> &mut CSVProxyListBuilder;
+    fn set_field_pos(&mut self, field: ProxyField, pos: u32) -> &mut CSVProxyListBuilder;
+
+    fn set_separator(&mut self, separator: String) -> &mut CSVProxyListBuilder;
+
+    fn build(&self) -> Vec<SimpleProxy>;
+}
+
+#[derive(Default)]
+pub struct CSVProxyListBuilder {
+
+    source: String,
+
+    parts_pos: HashMap<ProxyField, usize>,
+    fields: [ProxyField; 4],
+    separator: String,
+
+}
+
+impl FromCSVBuilder for CSVProxyListBuilder {
+    fn set_source(&mut self, file_or_url: String) -> &mut CSVProxyListBuilder {
+        self.source = file_or_url;
+
+        self
+    }
+
+    fn set_separator(&mut self, separator: String) -> &mut CSVProxyListBuilder {
+        self.separator = separator;
+        self
+    }
+
+    fn set_field_pos(&mut self, field: ProxyField, pos: u32) -> &mut CSVProxyListBuilder {
+        
+        
+        self.parts_pos.insert(field, pos as usize);
+        self
+
+    }
+
+    fn set_columns(&mut self, columns: [ProxyField; 4]) -> &mut CSVProxyListBuilder {
+        self.fields = columns;
+        self
+    }
+
+    fn build(&self) -> Vec<SimpleProxy> {
+        
+        let source_file = match self.source.starts_with("http") {
+
+            true => {
+                let mut resp = reqwest::blocking::get(&self.source).expect("Couldn't download the proxy list. You should provide a valid Proxy URL.");
+                
+                let mut out = File::create("config/proxies.csv").unwrap();
+
+                io::copy(&mut resp, &mut out).expect("failed to copy content");
+                out.sync_all().unwrap();
+                std::fs::File::open("config/proxies.csv").unwrap()
+
+            },
+            false => {
+                std::fs::File::open(&self.source).expect(format!("Proxy file not found: {}", &self.source).as_str())
+            }
+            
+        };
+
+        let host_pos = self.fields.iter().position(|e| *e == ProxyField::Host).expect("Port column should be defined.");
+        //let host_pos = self.parts_pos.get(&ProxyField::Host).expect("Host position should be defined.").to_owned();
+        //let port_pos = self.parts_pos.get(&ProxyField::Port).expect("Port position should be defined.").to_owned();
+        
+        let port_pos = self.fields.iter().position(|e| *e == ProxyField::Port).expect("Port column should be defined.");
+
+        let user_pos = self.fields.iter().position(|e| *e == ProxyField::Username).expect("Username column should be defined.");
+        
+        let password_pos = self.fields.iter().position(|e| *e == ProxyField::Password).expect("Password column should be defined.");
+        
+        let mut proxies: Vec<SimpleProxy> = vec![];
+        
+        let lines = std::io::BufReader::new(&source_file).lines();
+
+        for line in lines {
+            if let Ok(proxy_string) = line {
+                let parts = proxy_string.split(&self.separator).collect::<Vec<&str>>();
+
+                let p = SimpleProxy {
+                        host: parts.get(host_pos).unwrap().to_string(),
+                        port: parts.get(port_pos).unwrap().to_string(),
+                        user: parts.get(user_pos).unwrap().to_string(),
+                        password: parts.get(password_pos).unwrap().to_string(),
+                    };
+
+                proxies.push(p);
+
+            }
+        }
+
+        proxies
+
+    }
+}
 
 #[derive(Clone)]
 pub struct SimpleProxy {
--- src/webscrape/scraper.rs
+++ src/webscrape/scraper.rs
@@ -1,36 +1,39 @@
-use std::{sync::Arc, str::FromStr};
+use std::{str::FromStr, sync::Arc};
 
-use headless_chrome::{Browser, LaunchOptions, Tab, Element, browser::{transport::{Transport, SessionId}, tab::RequestPausedDecision}, protocol::cdp::{Fetch::{events::RequestPausedEvent, HeaderEntry, FulfillRequest, RequestPattern, RequestStage, FailRequest}, Network::ResourceType}};
-use rand::{rngs::{StdRng}, Rng, SeedableRng};
+use headless_chrome::{
+    browser::{
+        tab::RequestPausedDecision,
+        transport::{SessionId, Transport},
+    },
+    protocol::cdp::{
+        Fetch::{
+            events::RequestPausedEvent, FailRequest, FulfillRequest, HeaderEntry
+        },
+        Network::ResourceType,
+    },
+    Browser, Element, LaunchOptions, Tab,
+};
+use rand::{rngs::StdRng, Rng, SeedableRng};
 
-use super::{proxy::SimpleProxy};
-
-
+use super::proxy::SimpleProxy;
 
 pub trait Builder {
-
     fn set_proxies(&mut self, proxies: Vec<SimpleProxy>) -> &mut ScraperBuilder;
     fn set_default_timeout(&mut self, default_timeout: u64) -> &mut ScraperBuilder;
 
     fn build(&self) -> Scraper;
-    
 }
 
 #[derive(Default)]
 pub struct ScraperBuilder {
-
     pub proxies: Vec<SimpleProxy>,
-    pub default_timeout: u64
-
+    pub default_timeout: u64,
 }
 
 impl Builder for ScraperBuilder {
-
     fn set_default_timeout(&mut self, default_timeout: u64) -> &mut ScraperBuilder {
-
         self.default_timeout = default_timeout;
         self
-
     }
 
     fn set_proxies(&mut self, proxies: Vec<SimpleProxy>) -> &mut ScraperBuilder {
@@ -39,25 +42,21 @@ impl Builder for ScraperBuilder {
     }
 
     fn build(&self) -> Scraper {
-        
-        let browser = Browser::new(LaunchOptions{
-
+        let browser = Browser::new(LaunchOptions {
             //args: vec![&proxy_arg],
             headless: false,
             ..Default::default()
-        }).unwrap();
-        
+        })
+        .unwrap();
+
         let tab = browser.wait_for_initial_tab().unwrap();
-        
+
         tab.set_default_timeout(std::time::Duration::from_secs(5));
         tab.enable_fetch(None, None).unwrap();
 
         let proxies = self.proxies.clone();
         tab.enable_request_interception(Arc::new(
             move |_: Arc<Transport>, _: SessionId, intercepted: RequestPausedEvent| {
-                
-                let mut rng: StdRng = SeedableRng::from_entropy();
-
                 // !intercepted.params.request.url.ends_with(".jpg") && !intercepted.params.request.url.ends_with(".png") && !intercepted.params.request.url.ends_with(".js")
                 let v = intercepted.params.request.headers.0.unwrap();
 
@@ -66,44 +65,65 @@ impl Builder for ScraperBuilder {
                 //println!("{:?}", intercepted.params.resource_Type);
 
                 if intercepted.params.resource_Type == ResourceType::Document {
-                let rnd_i = rng.gen_range(0..proxies.len());
-                
-                let proxy = proxies.get(rnd_i).unwrap().to_owned();
-                
-                println!("{}", proxy.get_address());
-                let mut req_headers : Vec<HeaderEntry> = vec![];
-
-                let mut headers = reqwest::header::HeaderMap::new();
-
-                for (k,val) in headersmap{
-
-                    let myk = k.to_owned();
-                    let myv = val.to_owned();
-                    let myv = myv.to_string();
-
-                    let key = reqwest::header::HeaderName::from_str(&myk).unwrap();
-                    let vall = reqwest::header::HeaderValue::from_str(&myv);
-
-                    headers.insert(key, vall.unwrap());
-                }
-
-                req_headers.push(HeaderEntry {
-                    name: "Content-Type".to_string(),
-                    value: "text/html; charset=utf-8".to_string(),
-                });
-                //println!("running reqwest.. {}", intercepted.params.request.url);
-                let res = reqwest::blocking::Client::builder()
-                    .proxy(
-                        reqwest::Proxy::all(proxy.get_address())
-                            .unwrap()
-                            .basic_auth(&proxy.user, &proxy.password),
-                    )
-                    .default_headers(headers.to_owned())
-                    .build()
-                    .unwrap()
-                    .get(intercepted.params.request.url).send().unwrap().text().unwrap();
                     
-    
+                    
+                    let mut reqwest_proxy: Option<reqwest::Proxy> = None;
+                    if proxies.len() > 0 {
+                        let mut rng: StdRng = SeedableRng::from_entropy();
+                        let rnd_i = rng.gen_range(0..proxies.len());
+                        let proxy = proxies.get(rnd_i).unwrap().to_owned();
+                        println!("{}", proxy.get_address());
+
+                        reqwest_proxy = match proxy.user == "" {
+                            true => Some(reqwest::Proxy::all(proxy.get_address()).unwrap()),
+                            false => Some(
+                                        reqwest::Proxy::all(proxy.get_address())
+                                            .unwrap()
+                                            .basic_auth(&proxy.user, &proxy.password),
+                                    )
+                        };
+                    }
+
+                    let mut req_headers: Vec<HeaderEntry> = vec![];
+
+                    let mut headers = reqwest::header::HeaderMap::new();
+
+                    for (k, val) in headersmap {
+                        let myk = k.to_owned();
+                        let myv = val.to_owned();
+                        let myv = myv.to_string();
+
+                        let key = reqwest::header::HeaderName::from_str(&myk).unwrap();
+                        let vall = reqwest::header::HeaderValue::from_str(&myv);
+
+                        headers.insert(key, vall.unwrap());
+                    }
+
+                    req_headers.push(HeaderEntry {
+                        name: "Content-Type".to_string(),
+                        value: "text/html; charset=utf-8".to_string(),
+                    });
+
+                    //println!("running reqwest.. {}", intercepted.params.request.url);
+                    let req_builder = reqwest::blocking::Client::builder();
+
+                    let req_builder = match reqwest_proxy {
+                        Some(p) => req_builder.proxy(p),
+                        None => req_builder,
+                    };
+
+                    let client = req_builder
+                        .default_headers(headers.to_owned())
+                        .build()
+                        .unwrap();
+
+                    let res = client
+                        .get(intercepted.params.request.url)
+                        .send()
+                        .unwrap()
+                        .text()
+                        .unwrap();
+
                     let fulfill_request = FulfillRequest {
                         request_id: intercepted.params.request_id,
                         response_code: 200,
@@ -112,23 +132,32 @@ impl Builder for ScraperBuilder {
                         body: Some(base64::encode(res)),
                         response_phrase: None,
                     };
-    
+
                     RequestPausedDecision::Fulfill(fulfill_request)
                 } else if intercepted.params.resource_Type != ResourceType::Document {
                     RequestPausedDecision::Continue(None)
                 } else {
-                    RequestPausedDecision::Fail(FailRequest { request_id: intercepted.params.request_id, error_reason: headless_chrome::protocol::cdp::Network::ErrorReason::BlockedByClient })
-                    
+                    RequestPausedDecision::Fail(FailRequest {
+                        request_id: intercepted.params.request_id,
+                        error_reason:
+                            headless_chrome::protocol::cdp::Network::ErrorReason::BlockedByClient,
+                    })
                 }
             },
-        )).unwrap();
+        ))
+        .expect("You should check the validity of your proxies or the URL provided.");
 
-        Scraper { proxy: self.proxies.clone(), default_timeout: self.default_timeout, browser, tab, current_url: None }
+        Scraper {
+            proxy: self.proxies.clone(),
+            default_timeout: self.default_timeout,
+            browser,
+            tab,
+            current_url: None,
+        }
     }
 }
 
 pub struct Scraper {
-
     pub proxy: Vec<SimpleProxy>,
 
     pub default_timeout: u64,
@@ -140,29 +169,26 @@ pub struct Scraper {
 }
 
 impl Scraper {
-
     pub fn navigate_to(&mut self, url: &String) -> bool {
-
-
         self.tab.navigate_to(url).unwrap();
 
-        self.tab.wait_until_navigated().unwrap();
+        if let Err(_) = self.tab.wait_until_navigated() {
+            println!("Page load timeout..");
+        }
 
-        self.tab.wait_for_xpath_with_custom_timeout("//body", std::time::Duration::from_secs(5)).unwrap();
+        self.tab
+            .wait_for_xpath_with_custom_timeout("//body", std::time::Duration::from_secs(5))
+            .unwrap();
         self.current_url = Some(url.to_string());
-        
+
         return true;
-        
     }
 
     pub fn sleep(&mut self, seconds: u64) {
-
         std::thread::sleep(std::time::Duration::from_secs(seconds));
-        
     }
 
-    pub fn find_elements_by_css(&self, target: &String ) -> Vec<Element> {
-        
+    pub fn find_elements_by_css(&self, target: &String) -> Vec<Element> {
         if target.starts_with("/") {
             println!("Invalid CSS selector.");
             return vec![];
@@ -178,15 +204,13 @@ impl Scraper {
 
         let elements = match query_result {
             Ok(elements) => elements,
-            Err(_) => vec![]
+            Err(_) => vec![],
         };
 
         return elements;
-
     }
 
-    pub fn find_elements_by_xpath(&self, target: &String ) -> Vec<Element> {
-        
+    pub fn find_elements_by_xpath(&self, target: &String) -> Vec<Element> {
         if !target.starts_with("/") {
             println!("Invalid XPath selector.");
             return vec![];
@@ -202,11 +226,9 @@ impl Scraper {
 
         let elements = match query_result {
             Ok(elements) => elements,
-            Err(_) => vec![]
+            Err(_) => vec![],
         };
 
         return elements;
-
     }
-
 }
