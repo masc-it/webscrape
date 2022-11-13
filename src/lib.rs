37881770a54607c1ffa57288a498d1b2b0fd3700
masc-it <maurosciancalepore98@gmail.com>
2022-11-13 19:15:09 +0100

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
-mod scraper;
+mod scraping;
 mod pipeline;
 mod utils;
-mod html_only;
 
 pub mod proxy;
-pub use crate::scraper::{ScraperBuilder};
+pub use crate::scraping::chrome::{ScraperBuilder};
 
 pub use crate::pipeline::ScrapingPipeline;
 
 pub use crate::utils::{img_to_base64, save_screenshot};
 
-pub use crate::html_only::simple::SimpleScraper;
+pub use crate::scraping::simple::SimpleScraper;
