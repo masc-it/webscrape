e9d0845c6832694454b7888cf98a9ba001384f02
masc-it <maurosciancalepore98@gmail.com>
2022-11-13 19:05:01 +0100

    html only scraper WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod scraper;
 mod pipeline;
 mod utils;
+mod html_only;
 
 pub mod proxy;
 pub use crate::scraper::{ScraperBuilder};
@@ -8,3 +9,5 @@ pub use crate::scraper::{ScraperBuilder};
 pub use crate::pipeline::ScrapingPipeline;
 
 pub use crate::utils::{img_to_base64, save_screenshot};
+
+pub use crate::html_only::simple::SimpleScraper;
