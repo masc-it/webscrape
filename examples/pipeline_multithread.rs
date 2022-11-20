ddd209cc2b6f5516f395fbcd9a24261161892720
onipot <m.sciancalepore20@studenti.uniba.it>
2022-11-20 14:51:57 +0100

    multithread example tokio

--- examples/pipeline_multithread.rs
+++ examples/pipeline_multithread.rs
@@ -1,21 +1,22 @@
 use webscrape::{ScraperBuilder, ScrapingPipeline};
 use std::thread;
 
-pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
+#[tokio::main]
+async fn main() {
+
     let pipeline_file = std::env::args().nth(1).expect("no pipeline source given");
 
-    for i in 1..10 {
-        let pipeline_f = pipeline_file.clone();
-        thread::spawn(move || {
+    let p1 = pipeline_file.clone();
+
+    let handle = tokio::spawn(async move {
         
-            println!("hi number {} from the spawned thread!", i);
             let mut builder = ScraperBuilder::default();
             let scraper = builder.set_headless(false).set_default_timeout(5).build();
             
-            let mut pipeline = ScrapingPipeline::from_file(&pipeline_f, scraper);
+            let mut pipeline = ScrapingPipeline::from_file(&pipeline_file.clone(), scraper);
             let res = pipeline.run();
 
+            println!("YO BY 1");
             println!("num: {}", &res.elements.len());
         
             for (name, els) in res.elements {
@@ -30,12 +31,46 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
                     println!("--------------------");
                 }
             }
-        });
-    }
 
-    let mut line = String::new();
-    print!("Run : ");
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    });
+
+    let handle2 = tokio::spawn(async move {
+        
+        let mut builder = ScraperBuilder::default();
+        let scraper = builder.set_headless(false).set_default_timeout(5).build();
+        
+        let mut pipeline = ScrapingPipeline::from_file(&p1, scraper);
+        let res = pipeline.run();
+
+        println!("YO BY 2");
+        println!("num: {}", &res.elements.len());
     
-    Ok(())
+        for (name, els) in res.elements {
+    
+            println!("TARGET: {}", name);
+            for el in &els {
+                println!("{:?}", el.text);
+    
+                for (k, v) in &el.attrs {
+                    println!("{} - {}", k, v);
+                }
+                println!("--------------------");
+            }
+        }
+
+});
+
+    // Do some other work
+
+    let out = handle.await.unwrap();
+
+    handle2.await.unwrap();
+    
+    
 }
+
+/* let mut line = String::new();
+    print!("Run : ");
+    let b1 = std::io::stdin().read_line(&mut line).unwrap(); */
+
+    
